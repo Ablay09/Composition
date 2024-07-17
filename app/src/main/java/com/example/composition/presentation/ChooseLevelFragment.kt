af877552f55f44db3f96011afa6d492a43029371
abl <ablay.yedilbayev@kaspi.kz>
2024-07-17 19:55:38 +0500

    Prepare resources

---
+++ app/src/main/java/com/example/composition/presentation/ChooseLevelFragment.kt
@@ -0,0 +1,7 @@
+package com.example.composition.presentation
+
+import androidx.fragment.app.Fragment
+import com.example.composition.R
+
+class ChooseLevelFragment : Fragment(R.layout.fragment_choose_level) {
+}
