5d031d96b0a604c666811f37cb31b28fda23c511
abl <ablay.yedilbayev@kaspi.kz>
2024-07-17 19:55:38 +0500

    Prepare resources

---
+++ app/src/main/java/com/example/composition/presentation/GameFinishedFragment.kt
@@ -0,0 +1,7 @@
+package com.example.composition.presentation
+
+import androidx.fragment.app.Fragment
+import com.example.composition.R
+
+class GameFinishedFragment : Fragment(R.layout.fragment_game_finished) {
+}
---
+++ app/src/main/java/com/example/composition/presentation/WelcomeFragment.kt
@@ -0,0 +1,8 @@
+package com.example.composition.presentation
+
+import androidx.fragment.app.Fragment
+import com.example.composition.R
+
+class WelcomeFragment : Fragment(R.layout.fragment_welcome) {
+
+}
